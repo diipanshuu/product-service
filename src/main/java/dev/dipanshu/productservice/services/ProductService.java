2aef6cd8eacc9ce0860379f86fa1fd5b3dd741a4
diipanshuu <diipanshuu@gmail.com>
2024-04-17 16:06:34 +0530

    feat(api): Implement endpoint to fetch all products This commit introduces a new API endpoint to fetch all products. The ProductController now handles the '/products' endpoint, allowing clients to retrieve a collection of products. The FakeStoreProductService has been updated to fetch multiple products from the third-party API using RestTemplate. Additionally, the FakeStoreProductDto has been adjusted to accommodate the structure of the response containing multiple products. #ID02 BREAKING CHANGE: The API response format for fetching products has been updated to return a collection of products instead of a single product.

--- src/main/java/dev/dipanshu/productservice/services/ProductService.java
+++ src/main/java/dev/dipanshu/productservice/services/ProductService.java
@@ -1,7 +1,11 @@
 package dev.dipanshu.productservice.services;
 
+import dev.dipanshu.productservice.ProductServiceApplication;
 import dev.dipanshu.productservice.models.Product;
+
+import java.util.List;
 
 public interface ProductService {
     public Product getSingleProduct(Long id);
+    public List<Product> getProducts();
 }
